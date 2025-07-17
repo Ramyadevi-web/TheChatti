252af0da82590528adffed6a5c469c641fa3e156
Ramyadevi - web <ramyadevim7@gmail.com>
2025-07-17 13:02:02 +0530

    first commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container-fluid home-page d-flex justify-content-center align-items-center'>
+      <h2>Coming soon....</h2>
+    </div>
+  )
+}
+
+export default Home
